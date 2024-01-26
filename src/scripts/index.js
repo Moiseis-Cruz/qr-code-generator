e90601842ddae22bacb1eba0992f0a505759aa5b
Moiseis Cruz <moizzito@gmail.com>
2024-01-26 19:41:27 -0300

    I created the enter event.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,21 @@ let btnGenerateCode = document.getElementById("generate-code");
 
 let inputText = document.getElementById("text");
 
-let output = document.getElementById("output")
+let output = document.getElementById("output");
 
 btnGenerateCode.addEventListener("click", () => {
-    output.innerHTML = `<img id="img-code" src="https://api.qrserver.com/v1/create-qr-code/?data=${inputText.value}&amp;size=200x200" alt="QR Code">`
+
+    output.innerHTML = `<img id="img-code" src="https://api.qrserver.com/v1/create-qr-code/?data=${inputText.value}&amp;size=200x200" alt="QR Code">`;
+
+})
+
+inputText.addEventListener("keyup", (e) => {
+    const key = e.which || e.keyCode;
+
+    const isEnterKeyPressed = key === 13;
+
+    if(isEnterKeyPressed){
+        output.innerHTML = `<img id="img-code" src="https://api.qrserver.com/v1/create-qr-code/?data=${inputText.value}&amp;size=200x200" alt="QR Code">`;
+    };
+
 })
