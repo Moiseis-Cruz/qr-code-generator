c77efed61d5b38b20def456a36bcca48302a687d
Moiseis Cruz <moizzito@gmail.com>
2024-01-26 19:35:11 -0300

    I created the button event.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,9 @@
-let generateCode = document.getElementById("generate-code");
+let btnGenerateCode = document.getElementById("generate-code");
 
 let inputText = document.getElementById("text");
 
 let output = document.getElementById("output")
 
-generateCode.addEventListener("click", () => {
-    output.innerHTML = inputText.value
+btnGenerateCode.addEventListener("click", () => {
+    output.innerHTML = `<img id="img-code" src="https://api.qrserver.com/v1/create-qr-code/?data=${inputText.value}&amp;size=200x200" alt="QR Code">`
 })
